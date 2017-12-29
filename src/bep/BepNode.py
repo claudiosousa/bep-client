e7a1000320f050ad57159b2d76210ca701c6c8d1
Claudio <claudio.sousa@gmail.com>
2017-12-29 16:21:15 +0100

    Separate sending msg from reading message + accept multiple continous Index Update messages

--- src/bep/BepNode.py
+++ src/bep/BepNode.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import socket
+import select
 import ssl
 import struct
 from collections import namedtuple
@@ -21,6 +22,18 @@ def unpack_int(buf): return struct.unpack('>I', buf)[0]
 def unpack_short(buf): return struct.unpack('>H', buf)[0]
 
 
+# proto buff message types indexed by MessageType enum value
+ResponsesByTypeValue = [
+    protocol.ClusterConfig,  # MessageType 0
+    protocol.Index,  # MessageType 1
+    protocol.IndexUpdate,  # MessageType 2
+    protocol.Request,  # MessageType 3
+    protocol.Response,  # MessageType 4
+    protocol.DownloadProgress,  # MessageType 5
+    protocol.Ping,  # MessageType 6
+    protocol.Close  # MessageType 7
+]
+
 class BepNode:
     irequest = 0
     client_id = None
@@ -33,13 +46,13 @@ class BepNode:
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         context.check_hostname = False
         context.load_cert_chain(cert, key)
-        self.conn = context.wrap_socket(socket.socket(socket.AF_INET))
+        self.__conn = context.wrap_socket(socket.socket(socket.AF_INET))
 
     def connect(self, endpoint):
         """Connect to endpoint"""
 
-        self.conn.connect(endpoint)
-        self.conn.do_handshake()
+        self.__conn.connect(endpoint)
+        self.__conn.do_handshake()
 
     def hello(self, name):
         """Exchange hello's"""
@@ -53,13 +66,13 @@ class BepNode:
 
         hello_str = hello.SerializeToString()
 
-        self.conn.send(bytes([*pack_int(HELLO_MAGIC_NUMBER), *pack_short(len(hello_str)), *hello_str]))
+        self.__conn.send(bytes([*pack_int(HELLO_MAGIC_NUMBER), *pack_short(len(hello_str)), *hello_str]))
 
-        magic_nb = unpack_int(self.read(4))
+        magic_nb = unpack_int(self._read(4))
         assert HELLO_MAGIC_NUMBER == magic_nb, 'Hello must be prefixed by magic number'
 
-        msg_size = unpack_short(self.read(2))
-        hello.ParseFromString(self.read(msg_size))
+        msg_size = unpack_short(self._read(2))
+        hello.ParseFromString(self._read(msg_size))
         return hello
 
     def cluster_config(self, folders):
@@ -77,21 +90,27 @@ class BepNode:
             folder.ignore_delete = True
             folder.disable_temp_indexes = True
 
-        return self.send_msg(cluster_config, protocol.CLUSTER_CONFIG)
+        self._send_msg(cluster_config, protocol.CLUSTER_CONFIG)
+        return self._read_msg()
 
     def list_folder(self, folder='default'):
         """List a folder files"""
 
         index = protocol.IndexUpdate()
         index.folder = folder
-        share = self.send_msg(index, protocol.INDEX_UPDATE)
-        files = filter(lambda f: not f.deleted, share.files)
+        self._send_msg(index, protocol.INDEX_UPDATE)
+        files = []
+
+        # while there are messages to be read, keep reading it them
+        while select.select([self.__conn], [], [], 1)[0]:
+            share = self._read_msg()
+            files += filter(lambda f: not f.deleted, share.files)
+
         return {'folder': share.folder, 'files': files}
 
     def download_file(self, file, folder='default'):
         """Download a file"""
 
-        response = protocol.Response()
         file_content = b''
 
         request = protocol.Request()
@@ -103,7 +122,8 @@ class BepNode:
             request.offset = block.offset
             request.size = block.size
             self.irequest += 1
-            res = self.send_msg(request, protocol.REQUEST, response)
+            self._send_msg(request, protocol.REQUEST)
+            res = self._read_msg()
             assert res.code == protocol.NO_ERROR, f'Failed to retrieve file: {res.code}'
             file_content += res.data
 
@@ -113,17 +133,17 @@ class BepNode:
         """Upload file"""
         pass
 
-    def read(self, length):
+    def _read(self, length):
         """Reads fixed length of bytes from connection"""
 
         BUFFER_SIZE = 2 ** 12
 
         buffer = b''
         while len(buffer) < length:
-            buffer += self.conn.recv(min(length - len(buffer), BUFFER_SIZE))
+            buffer += self.__conn.recv(min(length - len(buffer), BUFFER_SIZE))
         return buffer
 
-    def send_msg(self, msg, msg_type, response=None):
+    def _send_msg(self, msg, msg_type):
         '''
          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -141,21 +161,24 @@ class BepNode:
         /                               /
         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
         '''
-        response = response or msg
-
         header = protocol.Header()
         header.type = msg_type
         header.compression = protocol.NONE
         header_str = header.SerializeToString()
 
         msg_str = msg.SerializeToString()
-        self.conn.send(bytes([*pack_short(len(header_str)), *header_str, *pack_int(len(msg_str)), *msg_str]))
+        self.__conn.send(bytes([*pack_short(len(header_str)), *header_str, *pack_int(len(msg_str)), *msg_str]))
 
-        header_size = unpack_short(self.read(2))
-        header.ParseFromString(self.read(header_size))
+    def _read_msg(self):
+        header = protocol.Header()
+        header_size = unpack_short(self._read(2))
+        header.ParseFromString(self._read(header_size))
 
-        msg_size = unpack_int(self.read(4))
-        res = self.read(msg_size)
+        response = ResponsesByTypeValue[header.type]()
+
+        msg_size = unpack_int(self._read(4))
+        res = self._read(msg_size)
+
         if header.compression == protocol.LZ4:
             res = res[3::-1] + res[4:]  # message int size header must be converted to little endian
             res = lz4.decompress(res)
@@ -176,6 +199,7 @@ def certificate_id(certfile):
     alphabet_dict = {l: i for i, l in enumerate(alphabet)}
 
     def luhn_number(part):
+        'https://en.wikipedia.org/wiki/Luhn_algorithm'
         total = 0
         for i, v in enumerate(alphabet_dict[x] for x in part):
             if i % 2:
